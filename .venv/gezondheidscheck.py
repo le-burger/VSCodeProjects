03218e71a66f5c1f03295387cc5f535029c1d778
Leander Burger <leanderburger@hotmail.com>
2020-10-04 16:10:08 +0200

    verbeteringen van mathieu doorgevoerd

--- .venv/gezondheidscheck.py
+++ .venv/gezondheidscheck.py
@@ -1,13 +1,13 @@
 # Leander Burger | program which asks for health stats and returns if these are healthy values.  
 
-# function used to check stats
+# Function used to check stats
 def isTussen(value, minimum, maximum):
     if value >= minimum and value <= maximum:
         return True
     else:
         return False
 
-# minimum and maximum health stat values
+# Minimum and maximum health stat values
 MAX_HR = 90
 MIN_HR = 55
 
@@ -17,27 +17,23 @@ MIN_TEMP = 36.3
 MAX_HG = 140
 MIN_HG = 100
 
-# asking for input from the user
+# Asking for input from the user
 userHeartRate = int(input("What is your current heartrate?"))
 userTemperature = int(input("What is your current temperature?"))
 userBloodpressure = int(input("What is your current bloodpressure?"))
 
-# stats are used below to check if user is healthy  
-healthyHeartRate = isTussen(userHeartRate, MIN_HR, MAX_HR)
-healthyTemperature = isTussen(userTemperature, MIN_TEMP, MAX_TEMP)
-healthyBloodpressure = isTussen(userBloodpressure, MIN_HG, MAX_HG)
-
-if healthyHeartRate: 
+# Stats are used below to check if user is healthy  
+if isTussen(userHeartRate, MIN_HR, MAX_HR): 
     print("You have a healthy heart rate!")
 else:
     print("Heart rate is to low. Go see the doctor")
 
-if healthyTemperature: 
+if isTussen(userTemperature, MIN_TEMP, MAX_TEMP): 
     print("You have a healthy temperature!")
 else:
     print("Temperature is to low. Go see the doctor")
 
-if healthyBloodpressure: 
+if isTussen(userBloodpressure, MIN_HG, MAX_HG): 
     print("You have a healthy blodpressure!")
 else:
     print("Bloodpressure is to low. Go see the doctor")
