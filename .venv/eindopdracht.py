fe4c07454c33eab57e396cafd0ddd8f2aab2cfb8
Leander Burger <leanderburger@hotmail.com>
2020-10-21 17:26:20 +0200

    Checked and done

--- .venv/eindopdracht.py
+++ .venv/eindopdracht.py
@@ -1,5 +1,5 @@
 # Auteur: Leander Burger 
-# Groep: ID1G1
+# Group: ID1G1
 # Eindopdracht
 # This program will show the progress of your study after collecting some data through questions.
 
@@ -52,30 +52,27 @@ studyPointsArray = []
 passedPointsArray = []
 
 x = 0
-
 for i in courses:
     courseArray.append(courses[x].value)
     studyPointsArray.append(studyPoints[x].value)
-    
     if passedCourse[x].value == 'Y':
         passedPointsArray.append(studyPoints[x].value)
-
     x += 1
 
 # Calculating time left for getting all points using datetime module
 endDateObj = datetime.strptime(endDate, "%d-%m-%Y")
 today = date.today()
 future = date(endDateObj.year,endDateObj.month,endDateObj.day)
-diff = future - today
+difference = future - today
 
-# function to calculate the progress
+# Function to calculate the progress
 def progress(total, passed):
     totalPoints = sum(total)
     passedPoints = sum(passed)
     PERCENTAGE = 100
     return (passedPoints / totalPoints) * PERCENTAGE
 
-# Output to user in a friendly way
+# Output in a friendly way
 def normal_print(s):
     for c in s:
         sys.stdout.write(c)
@@ -88,6 +85,7 @@ def slow_print(s):
         sys.stdout.flush()
         time.sleep(0.5)
 
-normal_print(f"After meticulously calculating and checking the gathered studypoints I've came to the conclusion that your progress to completion so far is {progress(studyPointsArray, passedPointsArray)}%!")
+# Output to user
+normal_print(f"After meticulously calculating and checking the gathered data I've came to the conclusion that your progress to completion so far is {progress(studyPointsArray, passedPointsArray)}%!")
 slow_print(".....")
-normal_print(f"No worries though you still have {diff.days} days left to get to 100%!")
+normal_print(f"No worries though you still have {difference.days} days left to get to 100%!")
